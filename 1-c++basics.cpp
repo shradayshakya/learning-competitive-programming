21be84dc5d38e3a0bc0b18a2b73a943c426394d2
shradayshakya <shradayshakya@lftechnology.com>
2026-01-26 14:46:46 +0545

    C++ basic notes

---
+++ 1-c++basics.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// cout << "World ";
+
+	// char c = 'a';
+	// int i = 1;
+	// double d = 3.5;
+	// bool b = 0;	
+	// long long l = 123123123;
+	// cout << c << endl << i << endl << d << endl << b << endl << l << endl;
+
+	// cout << 12 % 5 << endl;
+
+	// int n = 10;
+	// cout << n*2 << endl;
+
+	// string s1;
+	// string s2;
+	// string s3;
+	// cin >> s1 >> s2 >> s3;
+	// cout << s1 << endl << s2 << endl << s3;
+
+	// int c = 1;
+	// cout << c;
+
+	// char c = 'Z';
+	// cout << (int) c;
+
+	// int a;
+	// char c;
+	// cin >> c >> a;
+	// cout << c << " " << a;
+}
