f2a151350ccf7279f00ca35b7caad85d66d1ae26
shradayshakya <shradayshakya@lftechnology.com>
2026-01-27 14:34:55 +0545

    Add loops notes

---
+++ 4-c++basics-3.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	// int a = 3;
+	// int b = 4;
+
+	// if (a  > b ) {
+	// 	cout << "a is greater";
+	// } else if (b > a ) {
+	// 	cout << "b is greater";
+	// } else {
+	// 	cout << "a and b are equal";
+	// }
+
+	// for (int i = 1; i <= 10; cout <<  i++ << endl){
+	// }
+
+	// int t;
+	// cin >> t;
+
+	// while (t--) {
+	// 	int n;
+	// 	cin >> n;
+
+	// 	for (int i = 1; i <= n; i++) {
+	// 		for (int j = 1; j <= i; j++){
+	// 			cout << "*";
+	// 		}
+	// 		cout << endl;
+	// 	}
+	// }
+
+
+	int t = 10;
+	while(t--){
+		if (t == 8) {
+			continue;
+		}
+		cout << t << endl;
+	}
+}
