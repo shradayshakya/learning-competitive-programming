0a38f4fa28ba1379eaf1cba31c6e88fbd3936c13
shradayshakya <shradayshakya@lftechnology.com>
2026-02-05 10:50:52 +0545

    Study getline

--- 5-c++-basics-strings.cpp
+++ 5-c++-basics-strings.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main() {
-	string str = "Hello";
-	cout << str;
-	
+	// string str = "Hello";
+	// cout << str[str.length() - 1];
+
+
+	// string numberInLines;
+	// getline(cin, numberInLines);
+	// cout << numberInLines << endl;
+
+	int t;
+	cin >> t;
+	cin.ignore();
+	while (t--) {
+		string line;
+		getline(cin, line);
+		cout << line << endl;
+	}
 }
