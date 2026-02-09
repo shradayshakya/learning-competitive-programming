c6ed87b3013f475072e420c182c23ab6641c41d3
shradayshakya <shradayshakya@lftechnology.com>
2026-02-09 10:59:26 +0545

    Practice array

---
+++ 6-array.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	// int a[10];
+	// a[11] = 11;
+	// cout << a[11];
+
+	// int n;
+	// cin >> n;
+	// int data[n];
+
+	// for(int i = 0; i < n; i++) {
+	// 	cin >> data[i];
+	// }
+
+	// int sum = 0;
+
+	// for(int i = 0; i < n; i++) {
+	// 	sum += data[i];
+	// }
+
+	// cout << sum;
+
+	// int row, column;
+	// cin >> row >> column;
+
+	// int data[row][column];
+
+	// for (int i = 0; i < row; i++) {
+	// 	for (int j = 0; j < column; j++) {
+	// 		cin >> data[i][j];
+	// 	}
+	// }
+
+	// for (int i = 0; i < row; i++) {
+	// 	for (int j = 0; j < column; j++) {
+	// 		cout << data[i][j] << ' ';
+	// 	}
+	// 	cout << endl;
+	// }
+
+	int a_size = 1e7;
+	int data[a_size];
+	data[a_size - 1] = 10;
+	cout << data;
+
+}
