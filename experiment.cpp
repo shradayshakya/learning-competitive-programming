7c7b9f2d02dee9d15559b611656a555fcd66ad34
shradayshakya <shradayshakya@lftechnology.com>
2026-02-20 13:39:03 +0545

    Double pointers

--- experiment.cpp
+++ experiment.cpp
@@ -9,20 +9,42 @@ int main() {
 	// cout << a << endl;
 	// cout << &a << endl;
 
+	// int a;
+	// int *p_a;
+
+	// p_a = &a;
+	// a = 5;
+
+	// cout << p_a << endl;
+	// cout << *p_a << endl;
+
+	// *p_a = 6;
+
+	
+	// cout << p_a << endl;
+	// cout << *p_a << endl;
+
+	// int a[10]; 
+	// cout << &a << endl; 
+	// cout << &a[0] << endl; 
+	// cout << a + 1 << endl; 
+	// cout << &a[1] << endl; 
+	// cout << &a[2] << endl; 
+	// cout << &a[3] << endl; 
+	// cout << &a[4] << endl; 
+	// cout << &a[5] << endl; 
+	
 	int a;
-	int *p_a;
+	int *p;
+	int **dp;
+	p = &a;
+	dp = &p; 
 
-	p_a = &a;
-	a = 5;
-
-	cout << p_a << endl;
-	cout << *p_a << endl;
-
-	*p_a = 6;
-
-	
-	cout << p_a << endl;
-	cout << *p_a << endl;
-	
+	cout << a << endl;
+	cout << &a << endl;
+	cout << p << endl;
+	cout << &p << endl;
+	cout << dp << endl;
+	cout << &dp << endl;
 }
 
