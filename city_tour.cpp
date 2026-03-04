4d6687f0464a52300877a31976560a5c48cf15db
shradayshakya <shradayshakya@lftechnology.com>
2026-03-04 11:26:49 +0545

    Add notes for city tour

---
+++ city_tour.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		long n;
+		cin >> n;
+		long long output = n * 4;
+		cout << output << endl;
+	}
+}
