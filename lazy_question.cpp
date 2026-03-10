3622f9019d70b03315f8edd33b6799aa3faf5019
shradayshakya <shradayshakya@lftechnology.com>
2026-03-10 12:03:16 +0545

    Solve the too lazy to name the question problem

--- lazy_question.cpp
+++ lazy_question.cpp
@@ -7,30 +7,34 @@ int main () {
 	int a, b, c;
 	cin >> a >> b >> c;
 
-	int a_and_b_step_value = 0; 
-	int a_and_b_current_value = 0; 
 	int current_match = 0; 
-
 	int match_count = 0;
 	int current_number = 1;
+
 	while(match_count != c) {
 		bool is_divisible_by_a = current_number % a == 0;
 		bool is_divisible_by_b = current_number % b == 0;
-		bool is_divisible_by_a_and_b = is_divisible_by_a && is_divisible_by_b;
-		if (is_divisible_by_a_and_b || is_divisible_by_a || is_divisible_by_b) {
-			match_count++;
+		if (is_divisible_by_a || is_divisible_by_b) {
 			current_match = current_number;
+			match_count++;
 		} 
-
-		if (is_divisible_by_a_and_b) {
-			a_and_b_step_value = a_and_b_current_value;
-			a_and_b_current_value = current_number;
-		}
 		current_number++;
 	}
 
-	a_and_b_step_value = current_match == a_and_b_current_value ? a_and_b_step_value: a_and_b_current_value;
+	int step_number = current_match % a == 0 ? a : b;
 
-	cout << current_match << " " << a_and_b_step_value; 
-	
+	if (current_match % a == 0 && current_match % b == 0) {
+		int lcm_number = a < b ? a : b;
+
+		while (lcm_number % a != 0 || lcm_number % b != 0) {
+			lcm_number++;
+		}
+
+		step_number = lcm_number;
+	}
+
+	for (int i = current_match; i >= 0; i -= step_number) {
+
+		cout << i << ' ';
+	}
 }
