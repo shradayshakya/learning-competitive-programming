58e6e8d8b24213d3b5aac3cab47e1eae36de7a7d
shradayshakya <shradayshakya@lftechnology.com>
2026-03-11 11:17:23 +0545

    Move the step and lcm file

--- competitive-1/step_and_lcm.cpp
+++ competitive-1/step_and_lcm.cpp
@@ -7,23 +7,23 @@ int main () {
 	int a, b, c;
 	cin >> a >> b >> c;
 
-	int current_match = 0; 
+	int cth_number = 0; 
 	int match_count = 0;
-	int current_number = 1;
+	int current_number = 2;
 
 	while(match_count != c) {
 		bool is_divisible_by_a = current_number % a == 0;
 		bool is_divisible_by_b = current_number % b == 0;
 		if (is_divisible_by_a || is_divisible_by_b) {
-			current_match = current_number;
+			cth_number = current_number;
 			match_count++;
 		} 
 		current_number++;
 	}
 
-	int step_number = current_match % a == 0 ? a : b;
+	int step_number = cth_number % a == 0 ? a : b;
 
-	if (current_match % a == 0 && current_match % b == 0) {
+	if (cth_number % a == 0 && cth_number % b == 0) {
 		int lcm_number = a < b ? a : b;
 
 		while (lcm_number % a != 0 || lcm_number % b != 0) {
@@ -33,7 +33,7 @@ int main () {
 		step_number = lcm_number;
 	}
 
-	for (int i = current_match; i >= 0; i -= step_number) {
+	for (int i = cth_number; i >= 0; i -= step_number) {
 		cout << i << ' ';
 	}
 }
